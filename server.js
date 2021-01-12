19533b9822d38b13d5c5f38a0589ff5d57875cad
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-12 22:49:53 +0100

    Make a simple echo (to test dns.lookup())

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const cors = require('cors');
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');		// To verify a submitted url
+
+/* Connects to a database */
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+/* Mount a middleware that allows parsing of the payload of a POST request */
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // Basic Configuration
@@ -23,9 +30,27 @@ app.get('/api/hello', function(req, res) {
 });
 
 //APP
+app.post('/api/shorturl/new', (req, res, next) => {
+	let url = req.body.url;
+	const httpRegex = /^https?:\/\//i;		// Doesn't accept hostname with protocols
+	url = url.replace(httpRegex, "");		// Removed "http://" or "https://"
 
-
-
+	dns.lookup(url, function(err, address, family) {
+		if (err) {
+			req.error = err;
+		}
+		next();
+	});
+}, (req, res) => {
+	if (req.error) {
+		res.json({
+			error: "invalid url",
+			original_url: req.error.hostname
+		});
+	} else {
+		res.json({original_url: req.body.url});
+	}
+});
 
 
 
